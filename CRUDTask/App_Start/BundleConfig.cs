b8bd10648a5e33d35bd334b3c329ca4f490a289d
Abdallah Eliwa <abdalla.eliwa2@hotmail.com>
2020-09-20 03:24:13 +0200

    Configure project for design[Initial design]

--- CRUDTask/App_Start/BundleConfig.cs
+++ CRUDTask/App_Start/BundleConfig.cs
@@ -14,16 +14,19 @@ namespace CRUDTask
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/ajax").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/Main.js").Include(
+                      "~/Scripts/Main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bulma.min.css",
                       "~/Content/site.css"));
         }
     }
---
+++ CRUDTask/Scripts/Main.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function () {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= $("#header").outerHeight()) {
+            $("#header").removeClass("big-nav").addClass("small-nav");
+            $(".big-item").removeClass("big-item").addClass("small-item");
+            $(".navbar-burger").css("margin-top", "0%");
+            
+        }
+        else if (($(window).scrollTop() <= $("#header").outerHeight())) {
+            $("#header").removeClass("small-nav").addClass("big-nav");
+            $(".small-item").removeClass("small-item").addClass("big-item");
+            $(".navbar-burger").css("margin-top", "4%");
+        }
+    });
+
+
+    $("#account-image").click(function () {
+        $("#account-manager").toggleClass("is-active");
+
+    });
+
+
+    // for Drop Down
+
+    $("#user-name").click(function () {
+        $(".dropdown").toggleClass("is-active");
+    });
+});
